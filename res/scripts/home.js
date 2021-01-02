abfa4f916048456c2dab05902707468f418a4ff4
SteveSajeev <stevepsajeev2008@gmail.com>
2021-01-02 09:01:56 +0530

    Made  navbar partially responsive

--- res/scripts/home.js
+++ res/scripts/home.js
@@ -1,7 +1,12 @@
 
-// Changes the width of body to accurate width of viewport, some browsers include the scrollbar in 100vw
-var container =  document.getElementById("container");
-container.style.width = document.body.clientWidth+"px";
+
+function handleResize() {
+    // Changes the width of body to accurate width of viewport, some browsers include the scrollbar in 100vw
+    var container =  document.getElementById("container");
+    container.style.width = document.body.clientWidth+"px";
+}
+handleResize()
+window.addEventListener("resize", handleResize);
 
 
 // The below code is for the animation in home page, text changing  
